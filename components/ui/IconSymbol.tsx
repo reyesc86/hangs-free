99770264aabfebcc8a9c800a0c3a9e0c1e3ee1fb
Roman Batsenko <rbatsenko@gmail.com>
2025-04-03 17:23:59 +0200

    chore: fix copy icon on Android

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -18,6 +18,7 @@ const MAPPING = {
   "paperplane.fill": "send",
   "chevron.left.forwardslash.chevron.right": "code",
   "chevron.right": "chevron-right",
+  "doc.on.doc": "content-copy",
 } as Partial<
   Record<
     import("expo-symbols").SymbolViewProps["name"],
